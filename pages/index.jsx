84481a630a09c583d0f1462c97e681f27320df60
Joaquin Olivero <joaquinolivero97@gmail.com>
2022-05-03 04:08:21 -0300

    added favicon and title

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,14 @@
+import Head from 'next/head';
 import LandingBackground from "../components/LandingBackground/LandingBackground"
 import Home from "../components/Home/Home"
 
 export default function Index() {
   return (
     <div style={{ position: 'relative' }}>
+      <Head>
+        <link rel='shorcut icon' href='/codePDFicon.ico' />
+        <title>codePDF</title>
+      </Head>
       <Home />
       <LandingBackground />
     </div>
